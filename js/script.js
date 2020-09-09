d7ad8c67bac6469eaec98d0c14d0c0119b8a8870
Vasile Moraru <vasile.moraru@test.test>
2020-09-09 14:15:28 +0300

    0

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 "use strict"; 
 
-let number = 5;
-const leftBorderWidth = 1;
+//let number = 5;
+//const leftBorderWidth = 1;
+//
+////alert("vasea");
+//
+//const result = [];
+//result[0] = prompt("are you here", "DDDfsfsdfsd");
+//result[1] = prompt("are you here", "DDDfsfsdfsd");
+//result[2] = prompt("are you here", "DDDfsfsdfsd");
+//console.log(result);
+//console.log(`tra la la ${result}`);
 
-//alert("vasea");
-
-const result = [];
-result[0] = prompt("are you here", "DDDfsfsdfsd");
-result[1] = prompt("are you here", "DDDfsfsdfsd");
-result[2] = prompt("are you here", "DDDfsfsdfsd");
-console.log(result);
-console.log(`tra la la ${result}`);
