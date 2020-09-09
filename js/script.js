b13ce7ae6567b50cd016d60265b484028f83a4a9
Vasile Moraru <vasile.moraru@test.test>
2020-09-09 18:20:22 +0300

    vasaeaEste

--- js/script.js
+++ js/script.js
@@ -32,8 +32,7 @@ const a = prompt("film", ""),
     console.log(personalMovieDB);
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
-        sss
-        xxx
-        sss
-        xxx
+sdadas
+dsadas
+
     }
